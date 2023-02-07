50b10faa406303bcb513b621caa2a6e8e562183b
badrmatar <124414201+badrmatar@users.noreply.github.com>
2023-02-07 02:34:38 +0000

    Update urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from rango import views
 app_name = 'rango'
-<<<<<<< HEAD
-=======
 
->>>>>>> c2973e665a09b9ed026ccc6d656e7a1a25687726
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
