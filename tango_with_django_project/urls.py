c2973e665a09b9ed026ccc6d656e7a1a25687726
Bader Matar <2722762m0@student.gla.ac.uk>
2023-02-07 02:28:25 +0000

    yeah

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-    path('about/', views.about, name='about'),
+    
 ]
